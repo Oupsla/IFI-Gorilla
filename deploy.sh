782e3873e5bb42f2bdaeb13f4d93bb15c1a21ee4
Coenen Benjamin <benjamin.coenen@hotmail.com>
2016-11-23 14:48:51 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ IFS='  ' read -ra array <<< $goversion
 pkgSystemName="${array[@]: -1:1}"
 pkgSystemName=`echo "$pkgSystemName" | tr / _`
 
-pkgDirectory=$GOPATH/pkg/$pkgSystemName/$projectName
+pkgDirectory=$GOPATH/pkg/$pkgSystemName/github.com/oupsla/$projectName
 rm -Rf $pkgDirectory
 mkdir -p $pkgDirectory
 cp -r $generatedDirs $pkgDirectory
