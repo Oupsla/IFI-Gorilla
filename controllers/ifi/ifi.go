f38ffba209c20e7ea16f503ed4217b592194b7c8
Nicolas Delperdange <nicolas_delperdange@hotmail.com>
2016-11-28 09:05:26 +0100

    Commit finale solution

--- controllers/ifi/ifi.go
+++ controllers/ifi/ifi.go
@@ -18,3 +18,139 @@ func Welcome(res http.ResponseWriter, req *http.Request) {
 
 	utils.SendJSON(res, welcomeResponse)
 }
+
+func GetEnigma(res http.ResponseWriter, req *http.Request) {
+    response, err := blackbox.ExecuteFunction("getFirstEnigme", req.Method, nil)
+
+    if err != nil {
+        myError := utils.CustomError{StatusCode: 400, Message: err.Error()}
+        utils.SendError(res, myError)
+        return
+    }
+
+    utils.SendJSON(res, response)
+}
+
+type postParamsEnigma struct {
+  Result int `json:"result"`
+}
+
+func PostEnigma(res http.ResponseWriter, req *http.Request) {
+  params := &postParamsEnigma{}
+
+  err := utils.GetJSONContent(params, req)
+
+  if err != nil {
+    myError := utils.CustomError{StatusCode: 400, Message: err.Error()}
+    utils.SendError(res, myError)
+    return
+  }
+
+	response, err := blackbox.ExecuteFunction("submitFirstEnigme", req.Method, params.Result)
+
+	if err != nil {
+			myError := utils.CustomError{StatusCode: 400, Message: err.Error()}
+			utils.SendError(res, myError)
+			return
+	}
+
+	utils.SendJSON(res, response)
+
+}
+
+func GetPlage(res http.ResponseWriter, req *http.Request) {
+    response, err := blackbox.ExecuteFunction("getPlage", req.Method, nil)
+
+    if err != nil {
+        myError := utils.CustomError{StatusCode: 400, Message: err.Error()}
+        utils.SendError(res, myError)
+        return
+    }
+
+    utils.SendJSON(res, response)
+}
+
+type postParamPlage struct {
+  Result string `json:"result"`
+}
+
+func PostPlage(res http.ResponseWriter, req *http.Request) {
+  params := &postParamPlage{}
+
+  err := utils.GetJSONContent(params, req)
+
+  if err != nil {
+    myError := utils.CustomError{StatusCode: 400, Message: err.Error()}
+    utils.SendError(res, myError)
+    return
+  }
+
+	response, err := blackbox.ExecuteFunction("addSomethingToTheBeach", req.Method, params.Result)
+
+	if err != nil {
+			myError := utils.CustomError{StatusCode: 400, Message: err.Error()}
+			utils.SendError(res, myError)
+			return
+	}
+
+	utils.SendJSON(res, response)
+
+}
+
+func RemovePlage(res http.ResponseWriter, req *http.Request) {
+  params := &postParamPlage{}
+
+  err := utils.GetJSONContent(params, req)
+
+  if err != nil {
+    myError := utils.CustomError{StatusCode: 400, Message: err.Error()}
+    utils.SendError(res, myError)
+    return
+  }
+
+	response, err := blackbox.ExecuteFunction("removeSomethingToTheBeach", req.Method, params.Result)
+
+	if err != nil {
+			myError := utils.CustomError{StatusCode: 400, Message: err.Error()}
+			utils.SendError(res, myError)
+			return
+	}
+
+	utils.SendJSON(res, response)
+
+}
+
+func EditPlage(res http.ResponseWriter, req *http.Request) {
+  params := &postParamPlage{}
+
+  err := utils.GetJSONContent(params, req)
+
+  if err != nil {
+    myError := utils.CustomError{StatusCode: 400, Message: err.Error()}
+    utils.SendError(res, myError)
+    return
+  }
+
+	response, err := blackbox.ExecuteFunction("editSomethingToTheBeach", req.Method, params.Result)
+
+	if err != nil {
+			myError := utils.CustomError{StatusCode: 400, Message: err.Error()}
+			utils.SendError(res, myError)
+			return
+	}
+
+	utils.SendJSON(res, response)
+
+}
+
+func GetPresent(res http.ResponseWriter, req *http.Request) {
+    response, err := blackbox.ExecuteFunction("getPresent", req.Method, nil)
+
+    if err != nil {
+        myError := utils.CustomError{StatusCode: 400, Message: err.Error()}
+        utils.SendError(res, myError)
+        return
+    }
+
+    utils.SendJSON(res, response)
+}
--- routes/ifi/ifi.go
+++ routes/ifi/ifi.go
@@ -13,6 +13,41 @@ func GetRoutes() []routes.Route {
 		Method:      "GET",
 		Path:        "/welcome",
 		HandlerFunc: ifi.Welcome,
+	}, routes.Route{
+	  Name:        "2nd route",
+	  Method:      "GET",
+	  Path:        "/enigma", //L'url réel sera en fait /api/v1/myroute car tout est préfixé par /api/v1
+	  HandlerFunc: ifi.GetEnigma,
+	}, routes.Route{
+	  Name:        "3nd route",
+	  Method:      "POST",
+	  Path:        "/enigma", //L'url réel sera en fait /api/v1/myroute car tout est préfixé par /api/v1
+	  HandlerFunc: ifi.PostEnigma,
+	},routes.Route{
+	  Name:        "4nd route",
+	  Method:      "GET",
+	  Path:        "/plage", //L'url réel sera en fait /api/v1/myroute car tout est préfixé par /api/v1
+	  HandlerFunc: ifi.GetPlage,
+	},routes.Route{
+	  Name:        "5nd route",
+	  Method:      "POST",
+	  Path:        "/plage", //L'url réel sera en fait /api/v1/myroute car tout est préfixé par /api/v1
+	  HandlerFunc: ifi.PostPlage,
+	},routes.Route{
+	  Name:        "6nd route",
+	  Method:      "DELETE",
+	  Path:        "/plage", //L'url réel sera en fait /api/v1/myroute car tout est préfixé par /api/v1
+	  HandlerFunc: ifi.RemovePlage,
+	},routes.Route{
+	  Name:        "7nd route",
+	  Method:      "PUT",
+	  Path:        "/plage", //L'url réel sera en fait /api/v1/myroute car tout est préfixé par /api/v1
+	  HandlerFunc: ifi.EditPlage,
+	},routes.Route{
+	  Name:        "8nd route",
+	  Method:      "GET",
+	  Path:        "/present", //L'url réel sera en fait /api/v1/myroute car tout est préfixé par /api/v1
+	  HandlerFunc: ifi.GetPresent,
 	})
 
 	return Routes
